fe04af20befb16fa6a2d9857890fdeb686f90534
liushiyu0214 <liushiyu25@huawei.com>
2025-10-11 14:47:49 +0800

    修复头文件找不到

--- activation/ge_glu_grad_v2/examples/test_aclnn_ge_glu_backward.cpp
+++ activation/ge_glu_grad_v2/examples/test_aclnn_ge_glu_backward.cpp
@@ -11,7 +11,7 @@
 #include <vector>
 #include <unistd.h>
 #include "acl/acl.h"
-#include "aclnnop/level2/aclnn_geglu_backward.h"
+#include "aclnnop/clnn_geglu_backward.h"
 
 #define CHECK_RET(cond, return_expr) \
     do {                             \
