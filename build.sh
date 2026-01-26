31471af9be7c14d3345294ef1de82dd1bebb3c1e
yang-di52 <yangdi52@huawei.com>
2026-01-26 15:55:14 +0800

    -f、--run_example参数 支持组合experimental
    Co-authored-by: yang-di52<yangdi52@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !992 merge 8.5.0 into 8.5.0
    
    -f、--run_example参数 支持组合experimental
    
    Created-by: yang-di52
    Commit-by: yang-di52
    Merged-by: cann-robot
    Description: ## 描述
    当前build脚本的部分功能不支持experimental算子，增加-f、--run_example参数 支持组合--experimental，支持执行experimental算子的用例验证
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-nn!992

--- build.sh
+++ build.sh
@@ -1030,7 +1030,7 @@ set_ci_mode() {
   cd ${BASE_PATH}
   if [[ "$ENABLE_TEST" == "TRUE" ]]; then
     {
-      result=$(python3 ${BASE_PATH}/scripts/util/parse_changed_files.py ${CHANGED_FILES})
+      result=$(python3 ${BASE_PATH}/scripts/util/parse_changed_files.py ${CHANGED_FILES} ${ENABLE_EXPERIMENTAL})
     } || {
       echo $result && exit 1
     }
@@ -1041,7 +1041,7 @@ set_ci_mode() {
     done
   else
     {
-      result=$(python3 ${BASE_PATH}/scripts/util/parse_compile_changed_files.py ${CHANGED_FILES})
+      result=$(python3 ${BASE_PATH}/scripts/util/parse_compile_changed_files.py ${CHANGED_FILES} ${ENABLE_EXPERIMENTAL})
     } || {
       echo $result && exit 1
     }
@@ -1140,11 +1140,17 @@ build_example() {
     exit 1
   fi
 
+  local grep_word="-v"
+
+  if [[ "${ENABLE_EXPERIMENTAL}" == "TRUE" ]]; then
+    grep_word=""
+  fi
+
   OLDIFS=$IFS
   IFS=$'\n'
-  files=($(find ../ -path "*/${OP_NAME}/examples/${pattern}*.cpp"))
+  files=($(find ../ -path "*/${OP_NAME}/examples/${pattern}*.cpp" | grep ${grep_word} "experimental"))
   if [[ "$COMPUTE_UNIT" == "ascend910_95" ]]; then
-    files+=($(find ../ -path "*/${OP_NAME}/examples/arch35/${pattern}*.cpp"))
+    files=($(find ../ -path "*/${OP_NAME}/examples/arch35/${pattern}*.cpp" | grep ${grep_word} "experimental"))
   fi
   IFS=$OLDIFS
 
