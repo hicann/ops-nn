560a195654a0a4e40225c841e79a82c4cceaba88
liaohuming <liaohuming@huawei.com>
2026-07-15 09:20:39 +0800

    inplace_index modify review
    Co-authored-by: qq_45996812<liaohuming@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !7498 merge master into master
    
    inplace_index modify review
    
    Created-by: liaohuming
    Commit-by: qq_45996812
    Merged-by: cann-robot
    Description: ## 描述
    
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    aclnnIndexAddGetWorkspaceSize函数增加日志，确定性判断从==1换成!=0.
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    https://gitcode.com/cann/ops-math/issues/2232
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：
    
    ## AI/Agent生成声明
    <!--  [x] 表示选中 -->
    - [ ] AI辅助编写
    
    See merge request: cann/ops-nn!7498

--- index/inplace_scatter_add/op_host/op_api/aclnn_index_add.cpp
+++ index/inplace_scatter_add/op_host/op_api/aclnn_index_add.cpp
@@ -282,8 +282,10 @@ aclnnStatus aclnnIndexAddGetWorkspaceSize(const aclTensor* self, const int64_t d
     int64_t deterministicValue = 0;
     rtError_t retRts = aclrtGetSysParamOpt(ACL_OPT_DETERMINISTIC, &deterministicValue);
     if (retRts != RT_ERROR_NONE) {
+        OP_LOGD("aclrtGetSysParamOpt failed, fallback deterministicValue=0");
         deterministicValue = 0;
     }
+    OP_LOGD("deterministicValue=%ld", deterministicValue);
 
     // 固定写法，参数检查
     L2_DFX_PHASE_1(aclnnIndexAdd, DFX_IN(self, dim, index, source, alpha), DFX_OUT(out));
@@ -317,7 +319,7 @@ aclnnStatus aclnnIndexAddGetWorkspaceSize(const aclTensor* self, const int64_t d
     bool useNewOp = (GetCurrentPlatformInfo().GetSocVersion() == SocVersion::ASCEND910B ||
                      GetCurrentPlatformInfo().GetSocVersion() == SocVersion::ASCEND910_93 ||
                      (GetCurrentPlatformInfo().GetSocVersion() == SocVersion::ASCEND950 &&
-                      (deterministicValue == 1))) &&
+                      (deterministicValue != 0))) &&
                     dim == 0 && self->GetViewShape().GetDim(0) < MAX_SORT_SHAPE_DIM &&
                     (self->GetDataType() == op::DataType::DT_BF16 || self->GetDataType() == op::DataType::DT_FLOAT16) &&
                     (!(self->GetViewShape().GetDimNum() == 0 && index->GetViewShape().GetShapeSize() == 1));
