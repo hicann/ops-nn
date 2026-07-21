f590cb5925ab207886856d7085ae3759fd6f757f
sunchun <sunchun4@h-partners.com>
2026-07-21 15:56:43 +0800

    TF plugin开源
    Co-authored-by: sunchun<sunchun4@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !7516 merge nn into master
    
    TF plugin开源
    
    Created-by: sunchun
    Commit-by: sunchun
    Merged-by: cann-robot
    Description: ## 描述
    
    TF plugin开源修改
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-nn/issues/4196
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    ## AI/Agent生成声明
    <!--  [x] 表示选中 -->
    - [ ] AI辅助编写
    
    See merge request: cann/ops-nn!7516

---
+++ activation/elu_grad/framework/elu_grad_tf_plugin.cpp
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file elu_grad_tf_plugin.cpp
+ * \brief
+ */
+#include "register/register.h"
+
+namespace domi {
+// EluGrad maps directly from the TensorFlow op of the same name; auto operator mapping suffices.
+REGISTER_CUSTOM_OP("EluGrad")
+    .FrameworkType(TENSORFLOW)
+    .OriginOpType("EluGrad")
+    .ParseParamsByOperatorFn(AutoMappingByOpFn)
+    .ImplyType(ImplyType::TVM);
+} // namespace domi
---
+++ activation/log_softmax_grad/framework/log_softmax_grad_tf_plugin.cpp
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file log_softmax_grad_tf_plugin.cpp
+ * \brief
+ */
+#include "register/register.h"
+
+namespace domi {
+// LogSoftmaxGrad maps directly from the TensorFlow op of the same name; auto operator mapping suffices.
+REGISTER_CUSTOM_OP("LogSoftmaxGrad")
+    .FrameworkType(TENSORFLOW)
+    .OriginOpType("LogSoftmaxGrad")
+    .ParseParamsByOperatorFn(AutoMappingByOpFn)
+    .ImplyType(ImplyType::TVM);
+} // namespace domi
