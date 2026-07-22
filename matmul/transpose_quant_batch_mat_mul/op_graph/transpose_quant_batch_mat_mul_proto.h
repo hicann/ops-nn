e190c001d07f882b713b060332033c409ec7a424
jgx12 <jingaoxiang@huawei.com>
2026-07-22 10:58:47 +0800

    TransposeQuantBatchMatMul原型库资料部分缺失或者不准确,更改原型资料
    Co-authored-by: jgx12<jingaoxiang@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !7761 merge tqbmm_proto into master
    
    TransposeQuantBatchMatMul原型库资料部分缺失或者不准确,更改原型资料
    
    Created-by: jgx12
    Commit-by: jgx12
    Merged-by: cann-robot
    Description: ## 描述
    
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    TransposeQuantBatchMatMul原型库资料部分缺失或者不准确,更改原型资料
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    https://gitcode.com/cann/ops-nn/issues/4225
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    无
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    matmul/transpose_quant_batch_mat_mul/op_graph/transpose_quant_batch_mat_mul_proto.h
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [x] 文档更新
    - [ ] 其他，请描述：
    
    ## AI/Agent生成声明
    <!--  [x] 表示选中 -->
    - [x] AI辅助编写
    
    See merge request: cann/ops-nn!7761

--- matmul/transpose_quant_batch_mat_mul/op_graph/transpose_quant_batch_mat_mul_proto.h
+++ matmul/transpose_quant_batch_mat_mul/op_graph/transpose_quant_batch_mat_mul_proto.h
@@ -26,28 +26,42 @@ namespace ge {
 * float8_e4m3fn, float8_e5m2, hifloat8. 3D. Has format ND.
 * @li x2: A matrix tensor. Must be one of the following types:
 * float8_e4m3fn, float8_e5m2, hifloat8. 3D. Has format ND.
-* @li bias: An optional tensor. Bias for batchmatmul. Must be one of the following types:
-* float32, float16, bfloat16. 1D. Has format ND.
+* @li bias: An optional tensor. Reserved parameter, currently not supported.
 * @li x1_scale: A matrix tensor, quantization parameter.
              Must be one of the following types: float32、float8_e8m0、uint64_t. The format
-             supports ND. The shape is 1D (t,), with t equal to 1 or m, where m is the same as that of x1.
+             supports ND. \n
+             - In K-C quantification, the shape is 1D (m,), where m is the same as that of x1. \n
+             - In MX quantification, the shape is 4D (m, b, ceil(k / 64), 2), where m, b, k match those of x1. \n
+             - In T-C quantification, the shape can be empty or 1D (1,).
 * @li x2_scale: A matrix tensor, quantization parameter.
              Must be one of the following types: float32、float8_e8m0、uint64_t. The format
-             supports ND. The shape is 1D (t,), with t equal to n, where n is the same as that of x2.
+             supports ND. \n
+             - In K-C and T-C quantification, the shape is 1D (n,), where n is the same as that of x2. \n
+              - In MX quantification, the shape is 4D. When perm_x2 is [0, 1, 2], the shape is (b, ceil(k / 64), n, 2);
+                when perm_x2 is [0, 2, 1], the shape is (b, n, ceil(k / 64), 2), where b, k, n match those of x2.
 
 * @par Attributes:
 * Six attributes, including:
 * @li dtype: An int. Declare the output type, supports  1(float16), 27(bfloat16), 34(hifloat8).
 * @li group_size: An optional int. Indicating the ratio between x1_scale/x2_scale and x1/x2 in group dequantization.
-Default to be 0.
-* @li perm_x1: A list int. "x1" is permuted to shape [B, M, K] before multiplication, the default value is [1, 0, 2].
-* @li perm_x2: A list int. "x2" is permuted to shape [B, K, N] before multiplication, the default value is [0, 1, 2].
-* @li perm_y: A list int. "y" is permuted after multiplication, the default value is [1, 0, 2].
+* The group_size is composed of the group_size_m, group_size_n, and group_size_k, total occupying 48 bits.
+* 0-15 bits of group_size indicate group_size_k, 16-31 bits indicate group_size_n, 32-47 bits indicate group_size_m,
+* 48-63 bits of group_size are noneffective. \n
+* If any of group_size_m, group_size_n, group_size_k calculated by group_size is 0, recalculate it by
+* input shape, eg: group_size_m = m / scale_m (m % scale_m must be 0). \n
+* In MX quantification, group_size_m and group_size_n only support 0 or 1, and group_size_k only supports 32.
+* In K-C and T-C quantification, group_size only supports 0, other values do not take effect. Default to be 0.
+* @li perm_x1: A list int. "x1" is permuted to shape [B, M, K] before multiplication.
+* Supports [1, 0, 2], the default value is [1, 0, 2].
+* @li perm_x2: A list int. "x2" is permuted to shape [B, K, N] before multiplication.
+* In K-C quantification, supports [0, 1, 2]. In MX and T-C quantification, supports [0, 1, 2] or [0, 2, 1].
+* The default value is [0, 1, 2].
+* @li perm_y: A list int. "y" is permuted after multiplication. Supports [1, 0, 2], the default value is [1, 0, 2].
 * @li batch_split_factor: An optional int. Declares factor of output_batch. Default to be 1.
 
 * @par Outputs:
 * One output, including:
-* y: A matrix Tensor. Must be one of the following types: float16, bfloat16.
+* y: A matrix Tensor. Must be one of the following types: float16, bfloat16, hifloat8.
   The format supports ND. The shape dim must be 3D. \n
 */
 REG_OP(TransposeQuantBatchMatMul)
