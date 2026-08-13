6052b3c0e2b83cb7bf4e6c7954b5f8270d76edab
yu_qinfei <yuqinfei1@h-partners.com>
2026-08-13 16:35:42 +0800

    AnchorResponseFlags增加宏隔离，防止原型定义冲突
    Co-authored-by: yu_qinfei<yuqinfei1@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !8457 merge fix_anchorResponseFlags_reg into 9.1.0
    
    AnchorResponseFlags增加宏隔离，防止原型定义冲突
    
    Created-by: yu_qinfei
    Commit-by: yu_qinfei
    Merged-by: cann-robot
    Description: ## 描述
    
    AnchorResponseFlags增加宏隔离，防止原型定义冲突
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-nn/issues/4672
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    ## AI/Agent生成声明
    <!--  [x] 表示选中 -->
    - [ ] AI辅助编写
    
    See merge request: cann/ops-nn!8457

--- common/inc/op_graph/op_nn_proto_extend.h
+++ common/inc/op_graph/op_nn_proto_extend.h
@@ -899,25 +899,6 @@ REG_OP(AdaptiveMaxPool2d)
     .OP_END_FACTORY_REG(MovingSumWithSigmoid)
 
     /**
-     *@brief Generate the responsible flags of anchor in a single feature map.
-     *@par Inputs:
-     * gt_bboxes: Ground truth box, 2-D Tensor of type float32 with shape `[batch, 4]`.
-     *@par Attributes:
-     *@li featmap_size: The size of feature maps. It is a listint and size is 2.
-     *@li strides: Stride of current level, listint.
-     *@li num_base_anchors: The number of base anchors.
-     *@par Outputs:
-     * flags: The valid flags of each anchor in a single level, 1-D Tensor of type uint8.
-     */
-    REG_OP(AnchorResponseFlags)
-    .INPUT(gt_bboxes, TensorType({DT_FLOAT}))
-    .OUTPUT(flags, TensorType({DT_UINT8}))
-    .REQUIRED_ATTR(featmap_size, ListInt)
-    .REQUIRED_ATTR(strides, ListInt)
-    .REQUIRED_ATTR(num_base_anchors, Int)
-    .OP_END_FACTORY_REG(AnchorResponseFlags)
-
-    /**
     * @brief Anti quantizes the input .
     * @par Inputs:
     * @li x: A multi-dimensional tensor of type int8/int4, specifying the input.
