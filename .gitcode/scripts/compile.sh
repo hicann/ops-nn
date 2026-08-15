e1a6522c33df0b85d67a775d5afe8795c7a5ccb4
yajie_caroline <chenyajie6@huawei.com>
2026-08-15 16:27:40 +0800

    修改编译包名报错并更新冒烟脚本
    Co-authored-by: chenyajie<chenyajie6@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !8695 merge master into master
    
    修改编译包名报错并更新冒烟脚本
    
    Created-by: yajie_caroline
    Commit-by: chenyajie
    Merged-by: cann-robot
    Description: 修改编译包名报错并更新冒烟脚本
    
    See merge request: cann/ops-nn!8695

--- .gitcode/scripts/compile.sh
+++ .gitcode/scripts/compile.sh
@@ -123,34 +123,47 @@ case "${task_name}" in
             touch build_out/cann-ops-nn-950_linux-aarch64.run
         fi
         ;;
-esac
-
-case "${task_name}|${TARGET_BRANCH}" in
-    "Compile_Ascend_X86_mobile_station|master")
-        wget -nv https://kiri-obs.obs.cn-north-4.myhuaweicloud.com/Cann%20Large%20Model%20Foundation%208.5.0.rc002/cann-bisheng-compiler_9.2.0_linux-x86_64.run
-        chmod +x *.run
-        sudo -u jenkins ./*.run --full --quiet --install-path=/home/jenkins/Ascend
-        bash build.sh --pkg --soc=kirinx90 --cann_3rd_lib_path=/home/jenkins/opensource -j16
-        echo "exec cmd: [bash build.sh --pkg --soc=kirinx90 --cann_3rd_lib_path=/home/jenkins/opensource -j16]"
+    Compile_Ascend_X86_mobile_station)
+        if [ "${TARGET_BRANCH}" = "master" ];then
+            wget -nv https://kiri-obs.obs.cn-north-4.myhuaweicloud.com/Cann%20Large%20Model%20Foundation%208.5.0.rc002/cann-bisheng-compiler_9.2.0_linux-x86_64.run
+            chmod +x *.run
+            sudo -u jenkins ./*.run --full --quiet --install-path=/home/jenkins/Ascend
+            bash build.sh --pkg --soc=kirinx90 --cann_3rd_lib_path=/home/jenkins/opensource -j16
+            echo "exec cmd: [bash build.sh --pkg --soc=kirinx90 --cann_3rd_lib_path=/home/jenkins/opensource -j16]"
+        else
+            echo "not need build mobile_station"
+            mkdir build_out
+            touch build_out/cann-ops-nn-kirinx90_linux-x86_64.run
+            exit 0
+        fi
         ;;
-    *)
-        echo "not need build single"
-        mkdir single
-        touch single.tar.gz
+    Compile_Ascend_X86_mobile_station_ubuntu24)
+        if [ "${TARGET_BRANCH}" = "master" ];then
+            wget -nv https://kiri-obs.obs.cn-north-4.myhuaweicloud.com/Cann%20Large%20Model%20Foundation%208.5.0.rc002/cann-bisheng-compiler_9.2.0_linux-x86_64.run
+            chmod +x *.run
+            sudo -u jenkins ./*.run --full --quiet --install-path=/home/jenkins/Ascend
+            bash build.sh --pkg --soc=kirinx90 --cann_3rd_lib_path=/home/jenkins/opensource -j16
+            echo "exec cmd: [bash build.sh --pkg --soc=kirinx90 --cann_3rd_lib_path=/home/jenkins/opensource -j16]"
+        else
+            echo "not need build mobile_station"
+            mkdir build_out
+            touch build_out/cann-ops-nn-kirinx90_linux-x86_64.run
+            exit 0
+        fi
         ;;
-esac
-case "${task_name}|${TARGET_BRANCH}" in
-    "Compile_Ascend_X86_mobile_station_ubuntu24|master")
-        wget -nv https://kiri-obs.obs.cn-north-4.myhuaweicloud.com/Cann%20Large%20Model%20Foundation%208.5.0.rc002/cann-bisheng-compiler_9.2.0_linux-x86_64.run
-        chmod +x *.run
-        sudo -u jenkins ./*.run --full --quiet --install-path=/home/jenkins/Ascend
-        bash build.sh --pkg --soc=kirinx90 --cann_3rd_lib_path=/home/jenkins/opensource -j16
-        echo "exec cmd: [bash build.sh --pkg --soc=kirinx90 --cann_3rd_lib_path=/home/jenkins/opensource -j16]"
-        ;;
-    *)
-        echo "not need build single"
-        mkdir single
-        touch single.tar.gz
+    Compile_Ascend_X86_mobile_station_9030_ubuntu24)
+        if [ "${TARGET_BRANCH}" = "master" ];then
+            wget -nv https://kiri-obs.obs.cn-north-4.myhuaweicloud.com/Cann%20Large%20Model%20Foundation%208.5.0.rc002/cann-bisheng-compiler_9.2.0_linux-x86_64.run
+            chmod +x *.run
+            sudo -u jenkins ./*.run --full --quiet --install-path=/home/jenkins/Ascend
+            bash build.sh --pkg --soc=kirinx9030 --cann_3rd_lib_path=/home/jenkins/opensource -j16
+            echo "exec cmd: [bash build.sh --pkg --soc=kirinx90 --cann_3rd_lib_path=/home/jenkins/opensource -j16]"
+        else
+            echo "not need build mobile_station"
+            mkdir build_out
+            touch build_out/cann-ops-nn-kirinx90_linux-x86_64.run
+            exit 0
+        fi
         ;;
 esac
 
