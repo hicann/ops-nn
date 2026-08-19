6c2d0bbec3b8230246c5255a803e8dbb58d7148d
wow5523 <wuao1@huawei.com>
2026-08-19 11:29:58 +0800

    ASC接口兼容性整改：迁移repeat_interleave_grad内核接口名称
    Co-authored-by: wow5523<wuao1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !8850 merge asc-interface-migration into master
    
    ASC接口兼容性整改：迁移repeat_interleave_grad内核接口名称
    
    Created-by: zhu-xun
    Commit-by: wow5523
    Merged-by: cann-robot
    Description: ## 描述
    
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    ASC接口兼容性整改：迁移repeat_interleave_grad内核接口名称
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    ## AI/Agent生成声明
    <!--  [x] 表示选中 -->
    - [ ] AI辅助编写
    
    See merge request: cann/ops-nn!8850

--- index/repeat_interleave_grad/op_kernel/v35/repeat_interleave_grad_block_split_r.h
+++ index/repeat_interleave_grad/op_kernel/v35/repeat_interleave_grad_block_split_r.h
@@ -601,20 +601,20 @@ __aicore__ inline void RepeatInterleaveGradBlockSplitR<DataT, PromoteDataT, Inde
 
     __VEC_SCOPE__
     {
-        __local_mem__ PromoteDataT* dst = (__local_mem__ PromoteDataT*)dstTensor.GetPhyAddr();
-        __local_mem__ PromoteDataT* cah = (__local_mem__ PromoteDataT*)dstTensor.GetPhyAddr() + cacheID * stride;
-        __local_mem__ PromoteDataT* src = (__local_mem__ PromoteDataT*)srcTensor.GetPhyAddr();
+        __ubuf__ PromoteDataT* dst = (__ubuf__ PromoteDataT*)dstTensor.GetPhyAddr();
+        __ubuf__ PromoteDataT* cah = (__ubuf__ PromoteDataT*)dstTensor.GetPhyAddr() + cacheID * stride;
+        __ubuf__ PromoteDataT* src = (__ubuf__ PromoteDataT*)srcTensor.GetPhyAddr();
         uint32_t sreg = static_cast<uint32_t>(count);
         AscendC::MicroAPI::RegTensor<PromoteDataT> aReg, bReg;
         AscendC::MicroAPI::MaskReg pMask;
         for (uint16_t i = 0; i < outerLoopTimes; ++i) { // outerLoopTimes是dimA的大小
             pMask = AscendC::MicroAPI::UpdateMask<PromoteDataT>(sreg);
-            DataCopy(aReg, (__local_mem__ PromoteDataT*)src + i * outerLoopStride);
+            MicroAPI::LoadAlign(aReg, (__ubuf__ PromoteDataT*)src + i * outerLoopStride);
             for (uint16_t j = 0; j < innerLoopTimes; ++j) {
-                DataCopy(bReg, (__local_mem__ PromoteDataT*)dst + i * outerLoopStride + j * innerLoopStride);
+                MicroAPI::LoadAlign(bReg, (__ubuf__ PromoteDataT*)dst + i * outerLoopStride + j * innerLoopStride);
                 Add<PromoteDataT, AscendC::MicroAPI::MaskMergeMode::ZEROING>(aReg, aReg, bReg, pMask);
             }
-            DataCopy((__local_mem__ PromoteDataT*)cah + i * outerLoopStride, aReg, pMask);
+            MicroAPI::StoreAlign((__ubuf__ PromoteDataT*)cah + i * outerLoopStride, aReg, pMask);
         }
     }
 }
