04bfd85c9d394571ac34bd28ea6f90214af86eaf
liujie12345678 <liujie81@huawei.com>
2026-08-25 19:51:04 +0800

    修改add_layer_norm_quant_v2的examples用例
    Co-authored-by: liujie12345678<liujie81@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !9199 merge master into master
    
    修改add_layer_norm_quant_v2的examples用例
    
    Created-by: liujie12345678
    Commit-by: liujie12345678
    Merged-by: cann-robot
    Description: ## 描述
    
    修改add_layer_norm_quant_v2的examples用例
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-nn/issues/5035
    
    ## 测试
    测试验证OK
    
    ## 文档更新
    NA
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    ## AI/Agent生成声明
    <!--  [x] 表示选中 -->
    - [ ] AI辅助编写
    
    See merge request: cann/ops-nn!9199

--- norm/add_layer_norm_quant_v2/examples/test_aclnn_add_layer_norm_quant_v2.cpp
+++ norm/add_layer_norm_quant_v2/examples/test_aclnn_add_layer_norm_quant_v2.cpp
@@ -1,12 +1,11 @@
 /**
- * This program is free software, you can redistribute it and/or modify.
  * Copyright (c) 2026 Huawei Technologies Co., Ltd.
- * This file is a part of the CANN Open Software.
- * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
- * BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. See LICENSE in the root of
- * the software repository for the full text of the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
  */
 
 /*!
@@ -134,7 +133,8 @@ int main()
 
     std::vector<int64_t> xShape = {1, 32};
     std::vector<int64_t> gammaShape = {1, 32};
-    std::vector<int64_t> scaleShape = {1};
+    // scales/zeroPoints 需与 gamma 同形（per-channel 量化），A5 上 AddLayerNormQuant kernel 要求该约束
+    std::vector<int64_t> scaleShape = {1, 32};
 
     std::vector<int64_t> reduceShape = {1};
 
