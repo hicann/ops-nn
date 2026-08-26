785a07d7d5209a8d584dee5feec1b37caaa89676
Annyqw <yeqianwen2@huawei.com>
2026-08-26 11:05:56 +0800

    fix masked_scatter_sf
    Co-authored-by: Annyqw<yeqianwen2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !9231 merge fix0825 into 9.1.0
    
    fix masked_scatter_sf
    
    Created-by: Annyqw
    Commit-by: Annyqw
    Merged-by: cann-robot
    Description: ## 描述
    
    masked_scatter算子修改
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-nn/issues/5149
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    ## AI/Agent生成声明
    <!--  [x] 表示选中 -->
    - [ ] AI辅助编写
    
    See merge request: cann/ops-nn!9231

--- index/masked_scatter/op_api/aclnn_masked_scatter.cpp
+++ index/masked_scatter/op_api/aclnn_masked_scatter.cpp
@@ -505,6 +505,13 @@ aclnnStatus aclnnInplaceMaskedScatterGetWorkspaceSize(
         return ACLNN_SUCCESS;
     }
 
+    if (!Ops::NN::AclnnUtil::IsRegbase() && source->IsEmpty()) {
+        // 根据实际支持情况补充
+        *workspaceSize = 0;
+        uniqueExecutor.ReleaseTo(executor);
+        return ACLNN_SUCCESS;
+    }
+
     // 固定写法，将输入selfRef转换成连续的tensor
     auto selfRefContiguous = l0op::Contiguous(selfRef, uniqueExecutor.get());
     CHECK_RET(selfRefContiguous != nullptr, ACLNN_ERR_INNER_NULLPTR);
