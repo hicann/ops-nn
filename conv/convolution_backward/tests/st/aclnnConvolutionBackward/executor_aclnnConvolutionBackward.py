488237aa62a688807e79328bec7fcc12969d74a1
yejiani <yejiani2@huawei.com>
2026-09-04 17:50:59 +0800

    【质量加固】 修改函数名与文件名拼写错误
    Co-authored-by: yejiani<yejiani2@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !9882 merge quality into master
    
    【质量加固】 修改函数名与文件名拼写错误
    
    Created-by: yejiani
    Commit-by: yejiani
    Merged-by: cann-robot
    Description: ## 描述
    
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    【质量加固】 修改函数名与文件名拼写错误
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    https://gitcode.com/cann/ops-nn/issues/5569
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    函数名与文件名拼写正确
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    已更新
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [x] 文档更新
    - [ ] 其他，请描述：
    
    ## AI/Agent生成声明
    <!--  [x] 表示选中 -->
    - [ ] AI辅助编写
    
    See merge request: cann/ops-nn!9882

--- conv/convolution_backward/tests/st/aclnnConvolutionBackward/executor_aclnnConvolutionBackward.py
+++ conv/convolution_backward/tests/st/aclnnConvolutionBackward/executor_aclnnConvolutionBackward.py
@@ -18,6 +18,7 @@ from atk.tasks.api_execute.base_api import BaseApi
 from atk.tasks.api_execute.aclnn_base_api import AclnnBaseApi
 from atk.tasks.backends.lib_interface.acl_wrapper import AclFormat
 
+
 @register("golden_conv_backward")
 class AscendConvolutionBackwardInputApi(BaseApi):
     def __init__(self, task_result: TaskResult):
@@ -32,57 +33,66 @@ class AscendConvolutionBackwardInputApi(BaseApi):
             input_data.kwargs["gradOutput"].to(torch.float32),
             input_data.kwargs["input"].to(torch.float32),
             input_data.kwargs["weight"].to(torch.float32),
-            bias_sizes = input_data.kwargs["biasSizes"],
-            stride = input_data.kwargs["stride"],
-            padding = input_data.kwargs["padding"],
-            dilation = input_data.kwargs["dilation"],
-            transposed = input_data.kwargs["transposed"],
-            output_padding = input_data.kwargs["outputPadding"],
-            groups = input_data.kwargs["groups"],
-            output_mask = input_data.kwargs["outputMask"]
+            bias_sizes=input_data.kwargs["biasSizes"],
+            stride=input_data.kwargs["stride"],
+            padding=input_data.kwargs["padding"],
+            dilation=input_data.kwargs["dilation"],
+            transposed=input_data.kwargs["transposed"],
+            output_padding=input_data.kwargs["outputPadding"],
+            groups=input_data.kwargs["groups"],
+            output_mask=input_data.kwargs["outputMask"],
         )
-        if input_data.kwargs["outputMask"][0] == False:
-            grad_input = torch.zeros(input_data.kwargs["input"].shape).to(input_data.kwargs["input"].dtype)
-        if input_data.kwargs["outputMask"][1] == False:
-            grad_weight = torch.zeros(input_data.kwargs["weight"].shape).to(input_data.kwargs["weight"].dtype)
-        if input_data.kwargs["outputMask"][2] == False:
+        if not input_data.kwargs["outputMask"][0]:
+            grad_input = torch.zeros(input_data.kwargs["input"].shape).to(
+                input_data.kwargs["input"].dtype
+            )
+        if not input_data.kwargs["outputMask"][1]:
+            grad_weight = torch.zeros(input_data.kwargs["weight"].shape).to(
+                input_data.kwargs["weight"].dtype
+            )
+        if not input_data.kwargs["outputMask"][2]:
             if self.format == "NDHWC" or self.format == "NHWC":
                 channelIndex = len(input_data.kwargs["gradOutput"].shape) - 1
             else:
                 channelIndex = 1
-            grad_bias = torch.zeros(input_data.kwargs["gradOutput"].shape[channelIndex]).to(input_data.kwargs["gradOutput"].dtype)
+            grad_bias = torch.zeros(
+                input_data.kwargs["gradOutput"].shape[channelIndex]
+            ).to(input_data.kwargs["gradOutput"].dtype)
         if grad_input is not None:
             grad_input = grad_input.to(input_data.kwargs["gradOutput"].dtype)
         if grad_weight is not None:
             grad_weight = grad_weight.to(input_data.kwargs["gradOutput"].dtype)
         if grad_bias is not None:
             grad_bias = grad_bias.to(input_data.kwargs["gradOutput"].dtype)
-        
+
         return grad_input, grad_weight, grad_bias
 
     def get_format(self, input_data: InputDataset, index=None, name=None):
-            """
-            :param input_data: 参数列表
-            :param index: 参数位置
-            :param name: 参数名字
-            :return:
-            format at this index or name
-            """
-            if index == 2:
-                return AclFormat.ACL_FORMAT_ND
-            if input_data.kwargs["format"] == "NCHW":
-                return AclFormat.ACL_FORMAT_NCHW
-            elif input_data.kwargs["format"] == "NCL":
-                return AclFormat.ACL_FORMAT_NCL
-            elif input_data.kwargs["format"] == "ND":
-                return AclFormat.ACL_FORMAT_ND
-            elif input_data.kwargs["format"] == "NCDHW":
-                return AclFormat.ACL_FORMAT_NCDHW
+        """
+        :param input_data: 参数列表
+        :param index: 参数位置
+        :param name: 参数名字
+        :return:
+        format at this index or name
+        """
+        if index == 2:
+            return AclFormat.ACL_FORMAT_ND
+        if input_data.kwargs["format"] == "NCHW":
+            return AclFormat.ACL_FORMAT_NCHW
+        elif input_data.kwargs["format"] == "NCL":
+            return AclFormat.ACL_FORMAT_NCL
+        elif input_data.kwargs["format"] == "ND":
+            return AclFormat.ACL_FORMAT_ND
+        elif input_data.kwargs["format"] == "NCDHW":
+            return AclFormat.ACL_FORMAT_NCDHW
+
 
 @register("aclnn_conv_backward")
 class aclnnAscendConvolutionBackwardInputApi(AclnnBaseApi):
     def __init__(self, task_result: TaskResult, backend):
-        super(aclnnAscendConvolutionBackwardInputApi, self).__init__(task_result, backend)
+        super(aclnnAscendConvolutionBackwardInputApi, self).__init__(
+            task_result, backend
+        )
         self.input = None
 
     def init_by_input_data(self, input_data: InputDataset):
@@ -92,33 +102,45 @@ class aclnnAscendConvolutionBackwardInputApi(AclnnBaseApi):
         # input_args.pop()
         # atk不支持int8 json输入，在此转换
         import ctypes
+
         input_args[11] = ctypes.c_int8(input_data.kwargs["cubeMathType"])
         input_args[9] = ctypes.c_int(input_data.kwargs["groups"])
         self.input = input_data
         return input_args, output_packages
-    
+
     def after_call(self, output_packages):
         grad_input, grad_weight, grad_bias = super().after_call(output_packages)
-        
-        if self.input.kwargs["outputMask"][0] == False:
-            grad_input = torch.zeros(self.input.kwargs["input"].shape).to(self.input.kwargs["input"].dtype)
-        if self.input.kwargs["outputMask"][1] == False:
-            grad_weight = torch.zeros(self.input.kwargs["weight"].shape).to(self.input.kwargs["weight"].dtype)
-        if self.input.kwargs["outputMask"][2] == False:
-            if self.input.kwargs["format"] == "NDHWC" or self.input.kwargs["format"] == "NHWC":
+
+        if not self.input.kwargs["outputMask"][0]:
+            grad_input = torch.zeros(self.input.kwargs["input"].shape).to(
+                self.input.kwargs["input"].dtype
+            )
+        if not self.input.kwargs["outputMask"][1]:
+            grad_weight = torch.zeros(self.input.kwargs["weight"].shape).to(
+                self.input.kwargs["weight"].dtype
+            )
+        if not self.input.kwargs["outputMask"][2]:
+            if (
+                self.input.kwargs["format"] == "NDHWC"
+                or self.input.kwargs["format"] == "NHWC"
+            ):
                 channelIndex = len(self.input.kwargs["gradOutput"].shape) - 1
             else:
                 channelIndex = 1
-            grad_bias = torch.zeros(self.input.kwargs["gradOutput"].shape[channelIndex]).to(self.input.kwargs["gradOutput"].dtype)
-        
+            grad_bias = torch.zeros(
+                self.input.kwargs["gradOutput"].shape[channelIndex]
+            ).to(self.input.kwargs["gradOutput"].dtype)
+
         return grad_input, grad_weight, grad_bias
 
     def get_cpp_func_signature_type(self):
         # 校验函数
-        return "aclnnStatus aclnnAddmvGetWorkspaceSize(const aclTensor *gradOutput, \
+        return (
+            "aclnnStatus aclnnConvolutionBackwardGetWorkspaceSize(const aclTensor *gradOutput, \
             const aclTensor *input, const aclTensor *weight, const aclIntArray *biasSizes, \
             const aclIntArray *stride, const aclIntArray *padding, const aclIntArray *dilation, \
             bool transposed, const aclIntArray *outputPadding, int groups, \
             const aclBoolArray *outputMask, int8_t cubeMathType, \
             aclTensor *gradInput, aclTensor *gradWeight, aclTensor *gradBias, \
             uint64_t* workspaceSize, aclOpExecutor** executor)"
+        )
