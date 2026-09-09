00c296da5d912e5a929555e289af34f7944a09c7
LINxu233 <linxu5@huawei.com>
2026-09-09 17:58:37 +0800

    适配ops-tensor仓commitId 更新
    Co-authored-by: l00569907<linxu5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !10166 merge fix-ops-tensor-commitId into master
    
    适配ops-tensor仓commitId 更新
    
    Created-by: LINxu233
    Commit-by: l00569907
    Merged-by: cann-robot
    Description: ## 描述
    
    ops-tensor仓commitId 更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    ## AI/Agent生成声明
    <!--  [x] 表示选中 -->
    - [ ] AI辅助编写
    
    See merge request: cann/ops-nn!10166

--- cmake/third_party/ops-tensor.cmake
+++ cmake/third_party/ops-tensor.cmake
@@ -7,7 +7,7 @@
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
 # ----------------------------------------------------------------------------
-set(OPTENSOR_TAG_ID a60ed405bfd08b025eec853f0441030178b7878d)
+set(OPTENSOR_TAG_ID 781745c86312b478009742851b6cfc1967da8dda)
 
 if(EXISTS "${PROJECT_SOURCE_DIR}/../ops-tensor")
   get_filename_component(OPTENSOR_SOURCE_PATH
