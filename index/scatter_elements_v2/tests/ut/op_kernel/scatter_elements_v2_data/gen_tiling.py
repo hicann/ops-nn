312f8d7aec6e0000fe7d5d2daf813d1f86804eef
lihang_13123 <lihang106@h-partners.com>
2026-09-09 14:16:42 +0800

    修改nn仓部分算子license和format
    Co-authored-by: lihang_13123<lihang106@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !10082 merge scatter_nd_add into master
    
    修改nn仓部分算子license和format
    
    Created-by: lihang_13123
    Commit-by: lihang_13123
    Merged-by: cann-robot
    Description: ## 描述
    
    修改nn仓部分算子license和format
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-nn/issues/5706
    
    ## 测试
    冒烟OK
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：修改部分算子license和format
    
    ## AI/Agent生成声明
    <!--  [x] 表示选中 -->
    - [ ] AI辅助编写
    
    See merge request: cann/ops-nn!10082

--- index/scatter_elements_v2/tests/ut/op_kernel/scatter_elements_v2_data/gen_tiling.py
+++ index/scatter_elements_v2/tests/ut/op_kernel/scatter_elements_v2_data/gen_tiling.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-# coding: utf-8
-# This program is free software, you can redistribute it and/or modify.
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
 # Copyright (c) 2025 Huawei Technologies Co., Ltd.
-# This file is a part of the CANN Open Software.
-# Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
 # Please refer to the License for details. You may not use this file except in compliance with the License.
-# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
+# ----------------------------------------------------------------------------
 
 import numpy as np
-import torch
-import sys
 
 
 def gen_tiling():
-    variables_dict = {\
+    variables_dict = {
         "usedCoreNum": 43,
         "eachNum": 1,
         "extraTaskCore": 0,
@@ -49,7 +49,7 @@ def gen_tiling():
         "frontCoreData": 1,
         "tailCoreData": 1,
         "computeMode": 1,
-        "MLeft": 1
+        "MLeft": 1,
     }
 
     variables_array = [variables_dict[key] for key in variables_dict]
@@ -65,5 +65,5 @@ def main():
     base_params.tofile(tiling_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
