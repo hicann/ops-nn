a51e567b930f8a1b396ea640bc49f76bde399d42
VoyageZhou <zhouyuanhang4@h-partners.com>
2026-09-14 11:15:06 +0800

    迁移3个TF plugin
    Co-authored-by: akira<zhouyuanhang4@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !10288 merge tf_plugin_migration_from_canndev into master
    
    迁移3个TF plugin
    
    Created-by: VoyageZhou
    Commit-by: akira
    Merged-by: cann-robot
    Description: ## 描述
    迁移3个TF plugin
    - depthwise_conv2d_backprop_filter_plugin.cc
    - depthwise_conv2d_backprop_input_plugin.cc
    - depthwise_conv2d_forward_native_plugin.cc
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    [Issue 5812](https://gitcode.com/cann/ops-nn/issues/5812)
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    ## AI/Agent生成声明
    <!--  [x] 表示选中 -->
    - [x] AI辅助编写
    
    See merge request: cann/ops-nn!10288

---
+++ common/src/framework/depthwise_conv2d_backprop_input_tf_plugin.cpp
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file depthwise_conv2d_backprop_input_tf_plugin.cpp
+ * \brief
+ */
+#include <vector>
+#include "register/register.h"
+#include "framework/plugin_util.h"
+#include "graph/operator.h"
+#include "error_util.h"
+
+#include "log/log.h"
+
+namespace domi {
+namespace {
+const int32_t kInputFilter = 1;
+}
+
+/*!
+ * @brief Replace GE ParseParams fuction to process graph depthwise_conv2d_backprop_input node attrs
+ * @param opSrc the source op info from tf.
+ * @param op the dest GE op.
+ * @return status whether this operation success.
+ */
+static Status ParseParamsDepthwiseConv2DBackpropInput(const ge::Operator& opSrc, ge::Operator& op)
+{
+    if (AutoMappingByOpFn(opSrc, op) != SUCCESS) {
+        OP_LOGE(GetOpName(op).c_str(), "auto mapping failed.");
+        return FAILED;
+    }
+
+    ge::TensorDesc orgTensorW = op.GetInputDesc(kInputFilter);
+    orgTensorW.SetOriginFormat(ge::FORMAT_HWCN);
+    orgTensorW.SetFormat(ge::FORMAT_HWCN);
+    auto ret = op.UpdateInputDesc(kInputFilter, orgTensorW);
+    if (ret != ge::GRAPH_SUCCESS) {
+        OP_LOGE(GetOpName(op).c_str(), "update filter format failed.");
+        return FAILED;
+    }
+    OP_LOGD(GetOpName(op).c_str(), "update filter format success.");
+
+    std::vector<int32_t> padList = {0, 0, 0, 0};
+    op.SetAttr("pads", padList);
+    OP_LOGD(GetOpName(op).c_str(), "update pads success.");
+
+    return SUCCESS;
+}
+
+REGISTER_CUSTOM_OP("DepthwiseConv2DBackpropInput")
+    .FrameworkType(TENSORFLOW)
+    .OriginOpType("DepthwiseConv2dNativeBackpropInput")
+    .ParseParamsByOperatorFn(ParseParamsDepthwiseConv2DBackpropInput)
+    .ImplyType(ImplyType::TVM);
+} // namespace domi
